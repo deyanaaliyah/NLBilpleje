9499e7de9ebf98553f9e9b915c34ce2767b7cf53
deyanaaliyah <62359528+deyanaaliyah@users.noreply.github.com>
2020-11-23 12:24:49 +0100

    init

---
+++ Desktop/bilpleje/src/main/java/com/example/bilpleje/BilplejeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilpleje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilplejeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilplejeApplication.class, args);
+    }
+
+}
---
+++ Desktop/bilpleje/src/test/java/com/example/bilpleje/BilplejeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilpleje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilplejeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
