d575889372527f9cfb3d1cf97511b62cdab64e25
Mugisha98 <58075884+Mugisha98@users.noreply.github.com>
2020-12-02 15:16:18 +0100

    nye klasssenavne samt ændringer i controller

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,18 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Customer;
 import com.example.demo.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Optional;
+
+@RestController
 public class CustomerController {
 
     // Instantiates the person repository interface
     @Autowired
     private CustomerRepository repository;
 
+    /*
     // Reserves a customizable URL and body contains methods to be executed inside HTML
     @GetMapping("/customer")
     public String readPersons(Model model) {
@@ -20,6 +25,24 @@ public class CustomerController {
         // Prints out all persons on homepage via model.addAttribute method
        model.addAttribute("personToBeReturned", repository.findAll());
        return "/index.html";
+    }
+    */
+
+    // Reserves a customizable URL and body contains methods to be executed inside HTML
+    @GetMapping("/customer")
+    public Iterable<Customer> readCustomer(){
+        // Prints out all persons on homepage in Json format
+        return repository.findAll();
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<Optional<Customer>> readOne(@PathVariable Long id){
+        Optional<Customer> response = repository.findById(id);
+        if(response.isPresent()) {
+            return ResponseEntity.status(200).body(response);
+        }else {
+            return ResponseEntity.status(404).body(response);
+        }
     }
 
 
