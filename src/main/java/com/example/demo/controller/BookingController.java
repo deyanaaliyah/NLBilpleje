9a9cba3613d4da059e5fea51c58e84978dd61666
Oliver <oliverbramsen97@gmail.com>
2020-12-02 16:08:36 +0100

    BookingController

--- src/main/java/com/example/demo/controller/BookingController.java
+++ src/main/java/com/example/demo/controller/BookingController.java
@@ -1,8 +1,9 @@
 package com.example.demo.controller;
 
 
-import com.example.demo.model.Book;
-import com.example.demo.repository.BookRepository;
+
+import com.example.demo.model.Booking;
+import com.example.demo.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.awt.print.Book;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
@@ -19,7 +21,7 @@ public class BookingController {
 
     // Instantiates the person repository interface
     @Autowired
-    private BookRepository repository;
+    private BookingRepository repository;
 
     // Reserves a customizable URL and body contains methods to be executed inside HTML
     @GetMapping("/")
@@ -46,24 +48,24 @@ public class BookingController {
 
     //Post is equal to "I requested some things to be added/updated, now I tell the DB to handle and save my choices
     @PostMapping("/addEdit")
-    private String bookAddedOrUpdated(Book book) {
+    private String bookAddedOrUpdated(Booking booking) {
 
         //If an ID is not provided, then create/save a new one
-        if (book.getId() == null) {
-            repository.save(book);
+        if (booking.getId() == null) {
+            repository.save(booking);
         }
 
         //If ID is provided, then change these fields
         else{
             //Use Optional to find specific album to be updated
-            Optional<Book> book1 = repository.findById(book.getId());
+            Optional<Booking> book1 = repository.findById(booking.getId());
 
             if(book1.isPresent()){
-                Book editBook = book1.get();
-                editBook.setSize(book.getSize());
-                editBook.setAddition(book.getAddition());
-                editBook.setLicensePlate(book.getLicensePlate());
-                editBook.setCleaned(book.isCleaned());
+                Booking editBook = book1.get();
+                editBook.setSize(booking.getSize());
+                editBook.setAddition(booking.getAddition());
+                editBook.setLicensePlate(booking.getLicensePlate());
+                editBook.setStatus(booking.getStatus());
 
                 repository.save(editBook);
             }
@@ -76,10 +78,10 @@ public class BookingController {
     private String deleteBook(@PathVariable("id") Long id){
 
         //Optional to find specific album to be deleted
-        Optional<Book> book = repository.findById(id);
+        Optional<Booking> booking = repository.findById(id);
 
         //CrudRepository object will delete it
-        repository.deleteById(book.get().getId());
+        repository.deleteById(booking.get().getId());
 
         return "successful";
     }
